b7d1320bec7ec3afd5724644e1e8875a3d5ab998
mariolopp <mariolop.clah@gmail.com>
2023-11-16 20:31:04 +0100

    Cambios
    Se han añadido y comentado algunas cosas que creo que iban mal en el código

--- Assets/Scripts/GrupoI/AmplitudMovement.cs
+++ Assets/Scripts/GrupoI/AmplitudMovement.cs
@@ -44,52 +44,51 @@ namespace GrupoI
                 // Nodo en el que empieza el muñequito.
                 Nodo nodoInicial = new Nodo(_mundo, startNode, null);
                 
-                Nodo actual;
-                actual = _listaAbierta[0];
-                _listaAbierta.Add(actual);      // Añadimos el estado inicial a la lista abierta
-                
-                while(_listaAbierta.Count!=0) //Mientras la lista no esté vacía.
-                {
-                    // Coger el primer elemento de la lista abierta.
-                    actual = _listaAbierta[0];
+                Nodo actual;      // Comenzamos con el nodo inicial como nodo actual
+                //actual = _listaAbierta[0];      //  NO entiendo que pinta esto ?
+                _listaAbierta.Add(nodoInicial);      // Añadimos el estado inicial a la lista abierta
 
-                    // Metemos todos los nodos que hemos visitado en la lista cerrada.
-                    _listaCerrada.Add(actual);
-                    
-                    // Comprobar si es la meta.
-                    if(actual.esMeta())
+            while (_listaAbierta.Count != 0) //Mientras la lista no esté vacía.
+            {
+                // Coger el primer elemento de la lista abierta.
+                actual = _listaAbierta[0];
+                _listaAbierta.RemoveAt(0);  // Eliminamos dicho elemento de la lista abierta
+
+                // Metemos todos los nodos que hemos visitado en la lista cerrada.
+                _listaCerrada.Add(actual);
+
+                // Comprobar si es la meta.
+                if (actual.esMeta())
+                {
+                    //Devolver nodo meta.
+                }
+                else
+                {
+                    List<Nodo> nodosExpandidos = actual.expandirNodo();     // Lista de nodos expandidos a partir del actual
+
+                    //Meter nodosExpandidos en la lista abierta.
+                    foreach (Nodo nodo in nodosExpandidos)          // Recorre la lista nodosExpandidos
                     {
-                        //Devolver nodo meta.
-                    } 
-                    else 
-                    {
-                        List<Nodo> nodosExpandidos = actual.expandirNodo();     // Lista de nodos que se planean expandir
-                        //Cambiar expandirNodo para que devuelva una lista de Nodos
-                        //y no de CellInfos.
-                        
-                        //Meter nodosExpandidos en la lista abierta.
-                        foreach (Nodo nodo in nodosExpandidos)          // Recorre la lista nodosExpandidod
+                        for (int i = 0; i < _listaCerrada.Count; i++)
                         {
-                            for (int i = 0; i < _listaCerrada.Count; i++)
-                            {
-                            if (nodo != _listaCerrada[i]) {
-                                _listaAbierta.Add(nodo); //Lo añade al final. }
+                            if (nodo != _listaCerrada[i])
+                            {     // Añadimos unicamente si estos no existian ya en la lista cerrada
+                                _listaAbierta.Add(nodo);        // Lo añade al final.
                             }
                         }
                     }
 
                 }
-               
+            }
 
                 CellInfo[] path = new CellInfo[1];
 
                 CellInfo nextCell = conseguirVecino(startNode, _direccionActual); // Devuelve la celda vecina en la dirección indicada
                 
-                while (!nextCell.Walkable)
-                {
-                    
-                }
-                path[0] = nextCell;
+            //while (!nextCell.Walkable) {
+            //    path[0] = nextCell;
+            //}
+
                 return path;
             }
 
